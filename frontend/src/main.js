e927bb57e23e8db9b56b646278404518cccdeb0c
aarmad <aaron.madjri@efrei.net>
2026-02-21 00:50:19 +0100

    fix: use VITE_API_URL env variable for API base URL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 import './style.css'
 import App from './App.vue'
 
-axios.defaults.baseURL = 'http://localhost:8000/api'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 const token = localStorage.getItem('token')
 if (token) {
